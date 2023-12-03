a5fd272d3e8ac0896c0ae6c3cdffa3178dee714b
Dwafsigil <jtu02@jaguar.tamu.edu>
2023-12-03 03:21:51 -0600

    Final Changes
    The code only places warning when method is over 30+ lines. Does not detect public class as a method. Added a final line to recommend method refactoring.

--- src/main/java/LineCountError.java
+++ src/main/java/LineCountError.java
@@ -1,4 +1,5 @@
 import java.util.Stack;
+
 public class LineCountError {
 
     //Coding standard per the CS 307 and 305j Coding Standards
@@ -9,14 +10,19 @@ public class LineCountError {
     private String code = "";
     private String warnings = "";
     private int lineNumTemp;
+
+    private boolean firstBracketCheck = true;
+
     public LineCountError(String code) {
         this.code = code;
     }
+
     public String performMethodLengthCheck() {
         System.out.println(code);
         checkMethodLength();
         return warnings;
     }
+
     private void checkMethodLength() {
         Stack<Character> stack = new Stack<>();
         int lineNum = 1;
@@ -25,25 +31,39 @@ public class LineCountError {
         for (int i = 0; i < code.length(); i++) {
             char c = code.charAt(i);
 
+            //new line case
             if (c == '\n') { // if new line
                 lineNum++; // increment lineNum to match current line number
 
-            } else if (c == '{') { // if open bracket
-                if(stack.isEmpty()) { // if the stack is empty
-                    lineNumTemp = lineNum; // records start of the method in lineNumTemp
-                }
-                stack.push(c); // puts the open bracket into stack
+                // '{' case
+            } else if ((c == '{')) {
+                if(firstBracketCheck){ // ignores first bracket
+                    firstBracketCheck = false;
 
-            }else if (c == '}') { // if closed bracket
-                if(!stack.isEmpty() && stack.peek() == '{'){ // checks stack to see if open bracket
+                }else if (stack.isEmpty()) { // if the stack is empty
+                    lineNumTemp = lineNum; // records start of the method in lineNumTemp
+                    stack.push(c); // puts the open bracket into stack
+
+                }else if(stack.peek() == '{'){
+                   stack.push(c); //puts the open bracket into stack
+                }
+
+                // '}' case
+            } else if (c == '}') { // if closed bracket
+                if (!stack.isEmpty() && stack.peek() == '{') { // checks stack to see if open bracket
                     stack.pop(); // pops if open bracket
-                    if(stack.isEmpty()){
-                        warnings += "Method on line " + (lineNumTemp) + " has length of " + (lineNum - lineNumTemp + 1) + " lines\n";
+                    if (stack.isEmpty()) { //check if stack is empty after pop
+                        if ((lineNum - lineNumTemp + 1) > 30) { // if more than 20 lines
+                            warnings += "Method on line " + (lineNumTemp) + " has a length of " + (lineNum - lineNumTemp + 1) + " lines.\n";
+                        }
                     }
-                } else if(!stack.isEmpty() && stack.peek() == '}'){
+                } else if (!stack.isEmpty() && stack.peek() == '}') {
                     stack.push(c);
                 }
             }
+        }
+        if (warnings != "") { //if there are long methods, recommend refactor
+            warnings += "Please consider refactoring the method(s) above.\n";
         }
     }
 }

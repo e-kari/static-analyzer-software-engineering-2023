1c63e6a6c0cc05e84e67caddac119fc32ed323f5
wcsharpe <80623631+wcsharpe@users.noreply.github.com>
2023-12-03 18:41:10 -0600

    Revert "Adding example files"
    This reverts commit fbbf6137d494bedd685caff3cc419268107996fb.

--- target/classes/example5.java
+++
@@ -1,6 +0,0 @@
-public class example5 {
-    public static void main(String[] args) {
-        // No errors
-        System.out.print("This example file should have no errors.\n We're finally done with the project!".);
-    }
-}
